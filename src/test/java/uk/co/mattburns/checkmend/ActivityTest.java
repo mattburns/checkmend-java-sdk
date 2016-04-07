f7f2d0b51026be125f9e5d637eb823325692480a
Matt <matt@mattburns.co.uk>
2016-04-07 15:21:16 +0100

    Force timezone for sake of tests

--- src/test/java/uk/co/mattburns/checkmend/ActivityTest.java
+++ src/test/java/uk/co/mattburns/checkmend/ActivityTest.java
@@ -3,6 +3,7 @@ package uk.co.mattburns.checkmend;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.junit.Test;
 import uk.co.mattburns.checkmend.Activity;
@@ -11,10 +12,13 @@ public class ActivityTest {
 
     @Test
     public void can_serialize_to_json() {
+        // Overwrite the locale timezone for the sake of testing.
+        // Frankly this highlights the flaw that timezones are not supported.
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         Activity activity = new Activity(3, Activity.ActivityType.stolen, new Date(1));
 
         assertEquals(
-                "{\"propertyid\":3,\"activity\":\"stolen\",\"datetime\":\"1970-01-01 01:00:00\"}",
+                "{\"propertyid\":3,\"activity\":\"stolen\",\"datetime\":\"1970-01-01 12:00:00\"}",
                 activity.toJson());
     }
 
