b871042dd6adbcb76e386593e651b1d844a81fef
Matt <matt@mattburns.co.uk>
2016-04-21 15:49:42 +0100

    Tidyied up the readme and the test code to remove redundant imports.

--- README.md
+++ README.md
@@ -39,20 +39,6 @@ Error Handling
 Any call to the API could throw a CheckmendError which is an unchecked exception. It's up to you if you want to catch and handle it.
 
 
-Dependencies
-------------
-
-All dependencies are managed using the gradle dependency management system.
-
-
-
-Contribute/Extend
------------------
-
-Just import the project into [eclipse](http://www.eclipse.org/) or [intellij](https://www.jetbrains.com/idea/).
-If you want to play, you can build the jar from source using [gradle](http://www.gradle.org/). 
-
-
 More
 ----
 
@@ -62,9 +48,10 @@ See the [test code](https://github.com/mattburns/checkmend-java-sdk/blob/master/
 Secrets
 -------
 The following secrets are required for the Tests to work:
-    1. Checkmend Partner ID
-    2. Checkmend Persons ID
-    3. Checkmend Secret Key
+
+1. Checkmend Partner ID
+1. Checkmend Persons ID
+1. Checkmend Secret Key
 
 These are brought into the test system by the test-settings.properties file.
 
@@ -75,9 +62,9 @@ Currently this file is formatted in the following way:
 In this format, the system expects the property to be set somewhere in the build environment.
 These can be set in your build environment in the following ways:
 
-    1. As a gradle property (in ~/.gradle/gradle.properties) 
-    2. As a system property
-    3. As an environment variable
+1. As a gradle property (in `~/.gradle/gradle.properties`) 
+1. As a system property
+1. As an environment variable
     
 The TestUtils class will try and resolve them from all locations.
 
@@ -94,6 +81,6 @@ In this format, if the placeholder can't be resolved, the default value will be 
 Running the `gradle test` task will use the secrets as they have been set above. This is because the `test` task
 specifically sets gradle.properties values as system variables.
 
-However, when running a JUnit task (ctrl-r or ctrl-d in test class) that system won't work.
+However, when running a JUnit task (`ctrl-r` or `ctrl-d` in test class) that system won't work.
 
 Instead, edit your Run/Debug Configuration and add them as environment variables there.
--- src/test/java/uk/co/mattburns/checkmend/differentpackage/CheckmendTest.java
+++ src/test/java/uk/co/mattburns/checkmend/differentpackage/CheckmendTest.java
@@ -1,18 +1,14 @@
 package uk.co.mattburns.checkmend.differentpackage;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.mattburns.checkmend.*;
 
 import java.util.Date;
 import java.util.Properties;
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import uk.co.mattburns.checkmend.*;
-import uk.co.mattburns.checkmend.Activity.ActivityType;
-import uk.co.mattburns.checkmend.Property.Category;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CheckmendTest {
 
@@ -20,8 +16,7 @@ public class CheckmendTest {
     private long scfPersonid;
     private String secretKey;
 
-    @Before
-    public void before() {
+    @Before public void before() {
         Properties props = TestUtils.loadProps();
 
         partnerid = Long.parseLong((String) props.get("PARTNER_ID"));
@@ -29,32 +24,31 @@ public class CheckmendTest {
         secretKey = (String) props.get("SECRET_KEY");
     }
 
-    @Test
-    public void can_parse_error_json() {
+    @Test public void can_parse_error_json() {
         // @formatter:off
         CheckmendError error = Checkmend.jsonToCheckMENDError("" +
-       		"{" +
-       		"    \"errors\":" +
-       		"    [" +
-       		"        {" +
-       		"            \"id\": 1," +
-       		"            \"message\": \"Human readable message\"" +
-       		"        }," +
-       		"        {" +
-       		"            \"id\": 3," +
-       		"            \"message\": \"Another human readable message\"" +
-       		"        }" +
-       		"    ]" +
-       		"}");
+                "{" +
+                "    \"errors\":" +
+                "    [" +
+                "        {" +
+                "            \"id\": 1," +
+                "            \"message\": \"Human readable message\"" +
+                "        }," +
+                "        {" +
+                "            \"id\": 3," +
+                "            \"message\": \"Another human readable message\"" +
+                "        }" +
+                "    ]" +
+                "}");
         // @formatter:on
 
         assertEquals(1, error.getErrors().get(0).getId());
-        assertEquals("Human readable message", error.getErrors().get(0)
-                .getMessage());
+        assertEquals("Human readable message",
+                error.getErrors().get(0).getMessage());
 
         assertEquals(3, error.getErrors().get(1).getId());
-        assertEquals("Another human readable message", error.getErrors().get(1)
-                .getMessage());
+        assertEquals("Another human readable message",
+                error.getErrors().get(1).getMessage());
 
         // @formatter:off
         error = Checkmend.jsonToCheckMENDError("" +
@@ -63,30 +57,28 @@ public class CheckmendTest {
                 "    [" +
                 "        {" +
                 "            \"id\":210," +
-                "            \"message\":\"The required input: make was not provided.\"" +
+                "            \"message\":\"The required input: make was not provided.\""
+                +
                 "        }" +
                 "    ]" +
                 "}");
         // @formatter:on
 
         assertEquals(210, error.getErrors().get(0).getId());
-        assertEquals("The required input: make was not provided.", error
-                .getErrors().get(0).getMessage());
+        assertEquals("The required input: make was not provided.",
+                error.getErrors().get(0).getMessage());
     }
 
-    @Test
-    public void can_generate_signature_hash() {
+    @Test public void can_generate_signature_hash() {
         // example taken from http://gapi.checkmend.com/docs/api-usage.php
         String hash = Checkmend.generateSignatureHash(1, "234623ger787qws3423",
                 "{\"category\": 8}");
 
-        assertEquals(
-                "MTpmZjYwMjM2MDI2OTU1ODA3NDNmODRmZWI4ZjI3NTBkZTg5ZTBkM2Q2",
+        assertEquals("MTpmZjYwMjM2MDI2OTU1ODA3NDNmODRmZWI4ZjI3NTBkZTg5ZTBkM2Q2",
                 hash);
     }
 
-    @Test
-    public void can_register_person() {
+    @Test public void can_register_person() {
         Person bob = new Person.PersonBuilder("ref123").withFamilyname("smith")
                 .withOthernames("bob").build();
 
@@ -96,8 +88,7 @@ public class CheckmendTest {
         checkmend.removePerson(personid);
     }
 
-    @Test
-    public void removing_invalid_person_throws_exception() {
+    @Test public void removing_invalid_person_throws_exception() {
         Checkmend checkmend = new Checkmend(partnerid, secretKey, System.out);
         try {
             checkmend.removePerson(1);
@@ -105,13 +96,12 @@ public class CheckmendTest {
         } catch (CheckmendError e) {
             assertEquals(1, e.getErrors().size());
             assertEquals(801, e.getErrors().get(0).getId());
-            assertEquals("The Person ID supplied is invalid.", e.getErrors()
-                    .get(0).getMessage());
+            assertEquals("The Person ID supplied is invalid.",
+                    e.getErrors().get(0).getMessage());
         }
     }
 
-    @Test
-    public void can_register_property() {
+    @Test public void can_register_property() {
         Person bob = new Person.PersonBuilder("ref123").withFamilyname("smith")
                 .withOthernames("bob").build();
 
@@ -132,21 +122,20 @@ public class CheckmendTest {
         }
     }
 
-    @Test
-    public void can_encode_strings() {
+    @Test public void can_encode_strings() {
         Checkmend checkmend = new Checkmend(partnerid, secretKey, System.out);
 
         Property property = new Property.PropertyBuilder(scfPersonid,
-                Property.Category.Camera, "Canon", "0480417204").withDescription(
-                "colon : and other chars !\"£$%^&*()_+‽èก").build();
+                Property.Category.Camera, "Canon", "0480417204")
+                .withDescription("colon : and other chars !\"£$%^&*()_+‽èก")
+                .build();
 
         long propertyid = checkmend.registerProperty(property);
 
         checkmend.removeProperty(propertyid);
     }
 
-    @Test
-    public void can_register_activity() {
+    @Test public void can_register_activity() {
         Person bob = new Person.PersonBuilder("ref123").withFamilyname("smith")
                 .withOthernames("bob").build();
 
